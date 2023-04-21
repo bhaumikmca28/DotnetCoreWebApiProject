2da9866da75c37f49b1ac261ddf81518120242ed
Bhaumik Parekh <Bhaumik.Parekh@civica2.com>
2023-04-21 06:37:25 +0000

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DotnetCoreWebApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetCoreWebApiProject.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
